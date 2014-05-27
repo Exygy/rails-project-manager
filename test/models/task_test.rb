cdf9252958fcccf6f42e714d21f10db48f9b4424
Dominic Scotto <dominicscotto@gmail.com>
2014-05-27 14:06:48 -0700

    Added migration for tasks

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,5 +1,9 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
+  test "should not save task without task name" do
+    task = tasks(:first)
+    assert_not task.save, "Saved the task without a task name"
+  end
 
 end
