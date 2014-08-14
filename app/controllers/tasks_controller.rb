d9839c21ad2f1a1c25fd0228c37f3d848b80010c
David Stavis <davidstavis@gmail.com>
2014-08-14 12:46:48 -0700

    Tasks can be edited and destroyed from the project show page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,8 +21,34 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @project = Project.find(params[:project_id])
+    @task = @project.tasks.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:project_id])
+    @task = @project.tasks.find(params[:id])
+    @task.update(task_params)
+
+    if @task.save
+      redirect_to [@project], notice: 'Task was successfully updated.' 
+    else
+      render action: 'new', notice: 'Error, please try again'
+    end    
+  end
+
+  def destroy
+    @project = Project.find(params[:project_id])
+    @task = @project.tasks.find(params[:id])
+
+    @task.destroy
+    
+    redirect_to @project
+  end
+
   def task_params
-      params.require(:task).permit(:name, :description)
+      params.require(:task).permit(:name, :description, :difficulty_level)
   end
 
 end
